63654c8ee8064c8f1e024ceb24fcbc2f0b53adb3
miyamonz <m5813k@gmail.com>
2022-01-15 15:13:15 +0900

    open file and show by video tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [url, setUrl] = useState("");
 
-  return <div>hello</div>;
+  async function getFile() {
+    const fileHandle = await openFile();
+    const fileData = await fileHandle.getFile();
+    const src = URL.createObjectURL(fileData);
+    setUrl(src);
+  }
+
+  return (
+    <div>
+      hello
+      <button
+        onClick={() => {
+          getFile();
+        }}
+      >
+        open
+      </button>
+      <video controls src={url} style={{ width: "100vw" }}></video>
+    </div>
+  );
 }
 
 export default App;
+
+export async function openFile(): Promise<FileSystemFileHandle> {
+  const [fileHandle] = await window.showOpenFilePicker({
+    multiple: false,
+  });
+
+  return fileHandle;
+}
